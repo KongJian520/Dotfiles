19cf73f37470f168c74be0fec344cb29aeb447fb
KongJian <2501491361@qq.com>
2024-12-09 22:50:36 +0800

    更新底侧lualine,添加东京夜主题

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,56 +1,108 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  dependencies = { "nvim-tree/nvim-web-devicons" },  -- 如果想要图标的话需要这个依赖
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   init = function()
-    vim.o.laststatus = 3
-    vim.o.showmode = false
-    vim.o.ruler = false
-    vim.o.showcmd = false
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      vim.o.statusline = " "
+    else
+      vim.o.laststatus = 0
+    end
   end,
   opts = function()
-    local icons = {
-      diagnostics = {
-        Error = " ",
-        Warn = " ",
-        Hint = " ",
-        Info = " ",
+    local colors = require("config.color").colors
+    local mode_colors = require("config.color").mode_colors
+
+    -- 自定义主题
+    local custom_theme = {
+      normal = {
+        a = { fg = colors.dark, bg = colors.normal, gui = "bold" },
+        b = { fg = colors.fg, bg = colors.gray },
+        c = { fg = colors.fg, bg = colors.bg },
       },
-      git = {
-        added = " ",
-        modified = " ",
-        removed = " ",
+      insert = {
+        a = { fg = colors.dark, bg = colors.insert, gui = "bold" },
+      },
+      visual = {
+        a = { fg = colors.dark, bg = colors.visual, gui = "bold" },
+      },
+      replace = {
+        a = { fg = colors.dark, bg = colors.replace, gui = "bold" },
+      },
+      command = {
+        a = { fg = colors.dark, bg = colors.command, gui = "bold" },
+      },
+      inactive = {
+        a = { fg = colors.fg, bg = colors.bg },
+        b = { fg = colors.fg, bg = colors.bg },
+        c = { fg = colors.fg, bg = colors.bg },
       },
     }
 
-    return {
+    local icons = {
+      diagnostics = {
+        Error = " ",
+        Warn = " ",
+        Hint = " ",
+        Info = " ",
+      },
+      git = {
+        added = "+",
+        modified = "~",
+        removed = "-",
+      },
+    }
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    local opts = {
       options = {
-        theme = "auto",
-        globalstatus = true,
-        disabled_filetypes = { statusline = { "dashboard", "alpha" } },
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        theme = custom_theme,
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+        component_separators = { left = "", right = " " },
+        section_separators = { left = "", right = " " },
       },
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch" },
-        lualine_c = { "filename" },
-        lualine_x = { "diagnostics" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", path = 1 },
+        },
+        lualine_x = {
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+          },
+        },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
       },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = {},
+      extensions = { "neo-tree", "lazy" },
     }
+
+    return opts
   end,
-} 
+}
