97f62e447f6b36e21651df812a8472c3341e443f
KongJian <2501491361@qq.com>
2024-12-10 00:26:51 +0800

    变得好看了

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -11,6 +11,46 @@ return {
       "saadparwaiz1/cmp_luasnip" -- LuaSnip 的补全源
     },
     opts = function()
+      -- 添加高亮设置
+      local highlights = {
+        PmenuSel = { bg = "#282C34", fg = "NONE" },
+        Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
+        CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", strikethrough = true },
+        CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE", bold = true },
+        CmpItemAbbrMatchFuzzy = { fg = "#82AAFF", bg = "NONE", bold = true },
+        CmpItemMenu = { fg = "#C792EA", bg = "NONE", italic = true },
+        CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
+        CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
+        CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+        CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
+        CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
+        CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+        CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
+        CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
+        CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+        CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
+        CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+        CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
+        CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
+        CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
+        CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
+        CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
+        CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+      }
+
+      -- 应用高亮设置
+      for group, hl in pairs(highlights) do
+        vim.api.nvim_set_hl(0, group, hl)
+      end
+
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
       local defaults = require("cmp.config.default")()
@@ -59,49 +99,38 @@ return {
           { name = "buffer" },
         }),
         formatting = {
+          fields = { "kind", "abbr", "menu" },
           format = function(entry, item)
             local icons = {
-              Text = "󰉿",
-              Method = "󰆧",
-              Function = "󰊕",
-              Constructor = "",
-              Field = "󰜢",
-              Variable = "󰀫",
-              Class = "󰠱",
-              Interface = "",
-              Module = "",
-              Property = "󰜢",
-              Unit = "󰑭",
-              Value = "󰎠",
-              Enum = "",
-              Keyword = "󰌋",
-              Snippet = "",
-              Color = "󰏘",
-              File = "󰈙",
-              Reference = "󰈇",
-              Folder = "󰉋",
-              EnumMember = "",
-              Constant = "󰏿",
-              Struct = "󰙅",
-              Event = "",
-              Operator = "󰆕",
-              TypeParameter = "",
+              Text = '  ',
+              Method = '  ',
+              Function = '  ',
+              Constructor = '  ',
+              Field = '  ',
+              Variable = '  ',
+              Class = '  ',
+              Interface = '  ',
+              Module = '  ',
+              Property = '  ',
+              Unit = '  ',
+              Value = '  ',
+              Enum = '  ',
+              Keyword = '  ',
+              Snippet = '  ',
+              Color = '  ',
+              File = '  ',
+              Reference = '  ',
+              Folder = '  ',
+              EnumMember = '  ',
+              Constant = '  ',
+              Struct = '  ',
+              Event = '  ',
+              Operator = '  ',
+              TypeParameter = '  ',
             }
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-
-            local widths = {
-              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-            }
-
-            for key, width in pairs(widths) do
-              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
-              end
-            end
-
+            local kind_text = item.kind
+            item.kind = (icons[item.kind] or '')
+            item.menu = "    (" .. kind_text .. ")"
             return item
           end,
         },
@@ -111,6 +140,16 @@ return {
           } or false,
         },
         sorting = defaults.sorting,
+        window = {
+          completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+          },
+        },
+        view = {
+          entries = { name = 'custom', selection_order = 'near_cursor' }
+        },
       }
     end,
   },
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -61,7 +61,7 @@ return {
         theme = custom_theme,
         globalstatus = vim.o.laststatus == 3,
         disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-        component_separators = { left = "", right = " " },
+        component_separators = { left = "", right = " " },
         section_separators = { left = "", right = " " },
       },
       sections = {
