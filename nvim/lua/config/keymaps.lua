970aa8408aa425d484a12467ca4d5e1eb172cf1a
KongJian <2501491361@qq.com>
2024-12-09 22:00:28 +0800

    添加tree，更改keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,23 +4,24 @@ local map = vim.keymap.set
 vim.g.mapleader = " "
 
 -- 窗口控制快捷键
--- Ctrl + hjkl 切换窗口焦点
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+-- Ctrl + 方向键切换窗口焦点
+map("n", "<C-Left>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-Right>", "<C-w>l", { desc = "Go to right window" })
+map("n", "<C-Down>", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<C-Up>", "<C-w>k", { desc = "Go to upper window" })
 
--- Alt + hjkl 调整窗口位置
-map("n", "<A-h>", "<C-w>H", { desc = "Move window to the left" })
-map("n", "<A-l>", "<C-w>L", { desc = "Move window to the right" })
-map("n", "<A-j>", "<C-w>J", { desc = "Move window to the bottom" })
-map("n", "<A-k>", "<C-w>K", { desc = "Move window to the top" })
+-- Alt + 方向键调整窗口位置
+map("n", "<A-Left>", "<C-w>H", { desc = "Move window to the left" })
+map("n", "<A-Right>", "<C-w>L", { desc = "Move window to the right" })
+map("n", "<A-Down>", "<C-w>J", { desc = "Move window to the bottom" })
+map("n", "<A-Up>", "<C-w>K", { desc = "Move window to the top" })
 
--- Ctrl + Shift + hjkl 调整窗口大小
-map("n", "<C-S-h>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
-map("n", "<C-S-l>", ":vertical resize +2<CR>", { desc = "Increase window width" })
-map("n", "<C-S-j>", ":resize +2<CR>", { desc = "Increase window height" })
-map("n", "<C-S-k>", ":resize -2<CR>", { desc = "Decrease window height" })
+-- Ctrl + Shift + 方向键调整窗口大小
+map("n", "<A-S-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+map("n", "<A-S-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+map("n", "<A-S-Down>", ":resize +2<CR>", { desc = "Increase window height" })
+map("n", "<A-S-Up>", ":resize -2<CR>", { desc = "Decrease window height" })
+
 -- 基础快捷键
 map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,64 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("neo-tree.command").execute({ toggle = true })
+      end,
+      desc = "打开文件树",
+    },
+  },
+  opts = {
+    sources = { "filesystem", "buffers", "git_status" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["<Right>"] = "open",
+        ["<Left>"] = "close_node",
+        ["<space>"] = "none",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "复制路径到剪贴板",
+        },
+        ["O"] = {
+          function(state)
+            require("lazy.util").open(state.tree:get_node().path, { system = true })
+          end,
+          desc = "使用系统应用打开",
+        },
+        ["P"] = { "toggle_preview", config = { use_float = false } },
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- 如果设置为 nil 且启用文件嵌套，将启用展开器
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+      git_status = {
+        symbols = {
+          unstaged = "󰄱",
+          staged = "󰱒",
+        },
+      },
+    },
+  },
+} 
