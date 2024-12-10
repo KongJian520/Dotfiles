e600869a4ca3a0eee001a62c99d310c2beb67546
KongJian <2501491361@qq.com>
2024-12-10 14:54:10 +0800

    删除没用的部分

--- nvim/lua/plugins/lsp/python.lua
+++ nvim/lua/plugins/lsp/python.lua
@@ -18,7 +18,6 @@ return {
           },
         },
       },
-      update_in_insert = true,  -- 确保在插入模式下也显示诊断信息
     },
   },
   {
@@ -53,42 +52,41 @@ return {
   },
   {
     "linux-cultist/venv-selector.nvim",
-      dependencies = {
-        "neovim/nvim-lspconfig",
-        "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --both are optionals for debugging
-        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-      },
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python",   --both are optionals for debugging
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
     lazy = false,
     branch = "regexp", -- This is the regexp branch, use this for the new version
     config = function()
-  
-        -- This function gets called by the plugin when a new result from fd is received
-        -- You can change the filename displayed here to what you like.
-        -- Here in the example for linux/mac we replace the home directory with '~' and remove the /bin/python part.
-        local function shorter_name(filename)
-           return filename:gsub(os.getenv("HOME"), "~"):gsub("/bin/python", "")
-        end
-  
-  
-        require("venv-selector").setup {
-          settings = {
-            options = {
-              -- If you put the callback here as a global option, its used for all searches (including the default ones by the plugin)
+      -- This function gets called by the plugin when a new result from fd is received
+      -- You can change the filename displayed here to what you like.
+      -- Here in the example for linux/mac we replace the home directory with '~' and remove the /bin/python part.
+      local function shorter_name(filename)
+        return filename:gsub(os.getenv("HOME"), "~"):gsub("/bin/python", "")
+      end
+
+
+      require("venv-selector").setup {
+        settings = {
+          options = {
+            -- If you put the callback here as a global option, its used for all searches (including the default ones by the plugin)
+            on_telescope_result_callback = shorter_name
+          },
+
+          search = {
+            my_venvs = {
+              command = "fd /bin/python$ $WORKSPACE_PATH --full-path --color never -E /proc -unrestricted",
               on_telescope_result_callback = shorter_name
             },
-  
-            search = {
-              my_venvs = {
-               command = "fd /bin/python$ $WORKSPACE_PATH --full-path --color never -E /proc -unrestricted",
-               on_telescope_result_callback = shorter_name
-              },
-            },
           },
-        }
-      end,
-      keys = {
-        { "<leader>cv", "<cmd>VenvSelect<cr>" },
-      },
+        },
+      }
+    end,
+    keys = {
+      { "<leader>cv", "<cmd>VenvSelect<cr>" },
+    },
   },
   {
     "williamboman/mason.nvim",
