9dea99ca90f38a80407fd65ec4f8a98341db8edf
KongJian <2501491361@qq.com>
2024-12-10 11:37:14 +0800

    重构按键

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 local map = vim.keymap.set
-
+ 
 -- 设置 leader 键
 vim.g.mapleader = " "
 
@@ -25,3 +25,20 @@ map("n", "<A-S-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
 -- 基础快捷键
 map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+
+-- LSP 按键绑定
+local lsp_opts = { noremap = true, silent = true }
+map("n", "gD", vim.lsp.buf.declaration, lsp_opts)
+map("n", "gd", vim.lsp.buf.definition, lsp_opts)
+map("n", "K", vim.lsp.buf.hover, lsp_opts)
+map("n", "gi", vim.lsp.buf.implementation, lsp_opts)
+map("n", "<C-k>", vim.lsp.buf.signature_help, lsp_opts)
+map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, lsp_opts)
+map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, lsp_opts)
+map("n", "<space>wl", function()
+  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, lsp_opts)
+map("n", "<space>D", vim.lsp.buf.type_definition, lsp_opts)
+map("n", "<space>rn", vim.lsp.buf.rename, lsp_opts)
+map("n", "<space>ca", vim.lsp.buf.code_action, lsp_opts)
+map("n", "gr", vim.lsp.buf.references, lsp_opts)
