6cc0d41b0767e43bcb13298575f19f58da71bc26
KongJian <2501491361@qq.com>
2024-12-10 11:32:24 +0800

    允许python虚拟环境

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require('gitsigns').setup({
+            -- 你的 gitsigns 配置选项
+        })
+    end
+}
--- nvim/lua/plugins/lsp/python.lua
+++ nvim/lua/plugins/lsp/python.lua
@@ -52,15 +52,42 @@ return {
   },
   {
     "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
-    opts = {
-      name = {
-        "venv",
-        ".venv",
-        "env",
-        ".env",
+      dependencies = {
+        "neovim/nvim-lspconfig",
+        "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --both are optionals for debugging
+        { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
       },
-    },
-    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "选择 Python 虚拟环境" } },
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+  
+        -- This function gets called by the plugin when a new result from fd is received
+        -- You can change the filename displayed here to what you like.
+        -- Here in the example for linux/mac we replace the home directory with '~' and remove the /bin/python part.
+        local function shorter_name(filename)
+           return filename:gsub(os.getenv("HOME"), "~"):gsub("/bin/python", "")
+        end
+  
+  
+        require("venv-selector").setup {
+          settings = {
+            options = {
+              -- If you put the callback here as a global option, its used for all searches (including the default ones by the plugin)
+              on_telescope_result_callback = shorter_name
+            },
+  
+            search = {
+              my_venvs = {
+               command = "fd /bin/python$ $WORKSPACE_PATH --full-path --color never -E /proc -unrestricted",
+               on_telescope_result_callback = shorter_name
+              },
+            },
+          },
+        }
+      end,
+      keys = {
+        { "<leader>cv", "<cmd>VenvSelect<cr>" },
+      },
   },
+  
 } 
